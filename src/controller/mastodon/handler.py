002d0a57090f776464fea885d350c3347ecf2cf3
Manuel cortez <manuel@manuelcortez.net>
2024-11-26 22:44:38 -0600

    Mastodon: Allow to create user timelines from community local buffers

--- src/controller/mastodon/handler.py
+++ src/controller/mastodon/handler.py
@@ -200,7 +200,10 @@ class Handler(object):
     def open_timeline(self, controller, buffer):
         if not hasattr(buffer, "get_item"):
             return
-        item = buffer.get_item()
+        if hasattr(buffer, "community_url"):
+            item = buffer.get_item_from_instance()
+        else:
+            item = buffer.get_item()
         if buffer.type == "user":
             users = [item.acct]
         elif buffer.type == "baseBuffer":
